74139e14fcb3c145b7f9fed1d372f666c92b3095
Xin Wang <xin@xinwang.me>
2022-02-17 21:54:34 -0800

    added favico and head website title

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/theme";
@@ -6,6 +7,11 @@ import "@fontsource/inter";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Xin Wang</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico/" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
