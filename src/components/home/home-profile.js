e90e05b860cf926e3feef945cd49b02b8d233376
Xin Wang <xin@xinwang.me>
2022-02-17 21:06:22 -0800

    accidentally mispelled an useful import

--- src/components/home/home-profile.js
+++ src/components/home/home-profile.js
@@ -7,11 +7,13 @@ import {
   Image,
   useMediaQuery,
   Spacer,
+  useColorMode,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import React from "react";
 
 export default function HomeProfile() {
+  const { colorMode } = useColorMode();
   const [desktopQuery] = useMediaQuery("(min-width: 700px)");
   const [isMinWidth, setIsMinWidth] = useState(false);
 
